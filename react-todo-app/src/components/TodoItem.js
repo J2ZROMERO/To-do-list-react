70a345722dca7326550dac360e5a5b88e4a03a11
Jhosep <jose-zromero@hotmail.com>
2022-11-08 13:17:38 -0600

    Install Sass and update styles

--- react-todo-app/src/components/TodoItem.js
+++ react-todo-app/src/components/TodoItem.js
@@ -3,21 +3,25 @@ import styles from "./TodoItem.module.css"
 
 export default class TodoItem extends React.Component{
     render(){
-
-        return (
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+          }
+          const { completed, id, title } = this.props.todo
+          return (
             <li className={styles.item}>
-              <input
-                type="checkbox"
-                className={styles.checkbox}
-                checked={this.props.todo.completed}
-                onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-              />
-              <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-                Delete
-              </button>
-              {this.props.todo.title}
-            </li>
-          )
+            <input
+              type="checkbox"
+              className={styles.checkbox}
+              checked={completed}
+              onChange={() => this.props.handleChangeProps(id)}
+            />
+            <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+            <span style={completed ? completedStyle : null}>{title}</span>
+          </li>
+        )
 
 
     }
