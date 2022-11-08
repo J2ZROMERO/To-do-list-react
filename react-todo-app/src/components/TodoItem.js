3329ca3d82ce91b7e614053d599b3caf1429e84d
Jhosep <jose-zromero@hotmail.com>
2022-11-08 10:58:31 -0600

    Remove items completed

--- react-todo-app/src/components/TodoItem.js
+++ react-todo-app/src/components/TodoItem.js
@@ -8,6 +8,9 @@ export default class TodoItem extends React.Component{
   type="checkbox"
   checked={this.props.todo.completed}
   onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
+      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+  Delete
+</button>
       </li>
     ) 
 
--- react-todo-app/src/components/TodosList.js
+++ react-todo-app/src/components/TodosList.js
@@ -9,6 +9,7 @@ class TodosList extends React.Component {
            key={todo.id}
            todo={todo}
            handleChangeProps={this.props.handleChangeProps}
+           deleteTodoProps={this.props.deleteTodoProps}
          />
         ))}
       </ul>
