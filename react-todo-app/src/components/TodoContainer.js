87577d9c2d5de204fa1f4efa685baab381bcabbf
Jhosep <jose-zromero@hotmail.com>
2022-11-08 12:38:40 -0600

    Add trim method to check empty input

--- react-todo-app/src/components/TodoContainer.js
+++ react-todo-app/src/components/TodoContainer.js
@@ -2,21 +2,22 @@ import React from "react"
 import TodosList from './TodosList'
 import Header from "./Header"
 import InputTodo from "./InputTodo"
+import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
     state = {
         todos: [
           {
-            id: 1,
+            id: uuidv4(),
             title: "Setup development environment",
             completed: true
           },
           {
-            id: 2,
+            id: uuidv4(),
             title: "Develop website and add content",
             completed: false
           },
           {
-            id: 3,
+            id: uuidv4(),
             title: "Deploy to live server",
             completed: false
           }
