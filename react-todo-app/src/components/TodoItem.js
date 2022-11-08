e46ebce609b6859dfe688be761812af6ddd0c5e8
Jhosep <jose-zromero@hotmail.com>
2022-11-08 13:56:44 -0600

    Add life cycle methods

--- react-todo-app/src/components/TodoItem.js
+++ react-todo-app/src/components/TodoItem.js
@@ -5,6 +5,9 @@ export default class TodoItem extends React.Component{
     state = {
         editing: false,
       }
+      componentWillUnmount() {
+        console.log("Cleaning up...")
+      }
       handleEditing = () => {
         this.setState({
           editing: true,
